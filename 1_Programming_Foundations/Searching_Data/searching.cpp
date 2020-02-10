0b1f18c8c33a8dbcd0c233a2a1f6b7ccd65251b3
MAI THAI <delethai.mai@gmail.com>
2020-02-11 00:29:28 +0100

    Added findMax function

--- 1_Programming_Foundations/Searching_Data/searching.cpp
+++ 1_Programming_Foundations/Searching_Data/searching.cpp
@@ -11,15 +11,20 @@ int unOrderedSearch(std::vector<T> arrayVector, int value);
 template <typename T>
 int BinarySearch(std::vector<T> arrayVector, int n, int value);
 
+template <typename T>
+int findMax(std::vector<T> arrayVector);
+
+template <typename T>
+void disPlay(std::vector<T> vectorArray);
 
 
 // Main Functions
 
 main()
 {
-    std::vector<int> arrayVector = {6, 8, 19, 20, 23, 41, 49, 53, 56};
+    std::vector<int> arrayVector = {6, 8, 19, 20, 123, 78, 49, 53, 56};
     
-    std::cout << BinarySearch(arrayVector, arrayVector.size(), 41) << endl;
+    std::cout << findMax(arrayVector) << endl;
     return 0;
 }
 
@@ -51,6 +56,7 @@ int BinarySearch(std::vector<T> arrayVector, int n, int value) // n is the size 
     
 }
 
+
 /// searching on an unorered list
 template <typename T>
 int unOrderedSearch(std::vector<T> arrayVector, int value)
@@ -63,4 +69,42 @@ int unOrderedSearch(std::vector<T> arrayVector, int value)
         }
     }
     return 0;
+}
+
+
+/// finding max value in a list recursively
+template <typename T>
+int findMax(std::vector<T> arrayVector)
+{
+    if(arrayVector.size()==1)
+    {
+        return arrayVector[0];
+    }
+
+    int op1= arrayVector[0];
+    std::vector<T> newVector (arrayVector.cbegin() + 1, arrayVector.cend());
+    // disPlay(newVector);
+    int op2= findMax(newVector);
+
+    if (op1>op2)
+    {
+        return op1;
+    }
+    else
+    {
+        return op2;
+    }
+    
+}
+
+
+/// print vector
+template <typename T>
+void disPlay(std::vector<T> vectorArray)
+{
+    for (int i=0; i< vectorArray.size(); i++)
+    {
+        std::cout << vectorArray[i] << "\t";
+    }
+    std::cout << "\n";
 }
